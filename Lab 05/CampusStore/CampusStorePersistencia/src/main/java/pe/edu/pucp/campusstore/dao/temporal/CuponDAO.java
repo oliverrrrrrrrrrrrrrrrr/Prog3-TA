d2f8981b527b47da6518af5e61fdc6a24602da7b
oliverrrrrrrrrrrrrrrrr <oliraba2021@gmail.com>
2025-09-15 13:23:42 -0500

    persistencia

---
+++ Lab 05/CampusStore/CampusStorePersistencia/src/main/java/pe/edu/pucp/campusstore/dao/temporal/CuponDAO.java
@@ -0,0 +1,12 @@
+package pe.edu.pucp.campusstore.dao.temporal;
+
+import pe.edu.pucp.campusstore.dao.Persistible;
+import pe.edu.pucp.campusstore.modelo.temporal.Cupon;
+
+/**
+ *
+ * @author eric
+ */
+public interface CuponDAO extends Persistible<Cupon, Integer> {
+    
+}
