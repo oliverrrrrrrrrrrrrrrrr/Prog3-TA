c963962f9127e18f9bc64da605155988fb3ab0fa
oliverrrrrrrrrrrrrrrrr <oliraba2021@gmail.com>
2025-11-23 22:34:07 -0500

    rest editorial

--- CampusStore/CampusStore/CampusStoreWS/src/main/java/pe/edu/pucp/campusstorews/EditorialWS.java
+++ CampusStore/CampusStore/CampusStoreWS/src/main/java/pe/edu/pucp/campusstorews/EditorialWS.java
@@ -1,11 +1,20 @@
 package pe.edu.pucp.campusstorews;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.jws.WebService;
 import jakarta.jws.WebMethod;
 import jakarta.jws.WebParam;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.List;
+import java.util.ResourceBundle;
 import pe.edu.pucp.campusstore.bo.EditorialBO;
 import pe.edu.pucp.campusstore.boimpl.EditorialBOImpl;
+import pe.edu.pucp.campusstore.modelo.Articulo;
 import pe.edu.pucp.campusstore.modelo.Editorial;
 import pe.edu.pucp.campusstore.modelo.enums.Estado;
 
@@ -13,37 +22,93 @@ import pe.edu.pucp.campusstore.modelo.enums.Estado;
         serviceName = "EditorialWS",
         targetNamespace = "http://services.campusstore.pucp.edu.pe/")
 public class EditorialWS {
-    
-    private final EditorialBO editorialBO;
+    private final ResourceBundle config;
+    private final String urlBase;
+    private final HttpClient client = HttpClient.newHttpClient();
+    private final String NOMBRE_RESOURCE = "editoriales";
     
     public EditorialWS() {
-        this.editorialBO = new EditorialBOImpl();
+        this.config = ResourceBundle.getBundle("app");
+        this.urlBase = this.config.getString("app.services.rest.baseurl");
     }
     
     @WebMethod(operationName = "listarEditoriales")
-    public List<Editorial> listarEditoriales() {
-        return this.editorialBO.listar();
+    public List<Editorial> listarEditoriales() throws IOException, InterruptedException{
+        String url = this.urlBase + "/" + this.NOMBRE_RESOURCE;
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+        
+        HttpResponse<String> response = 
+                client.send(request, HttpResponse.BodyHandlers.ofString());
+        String json = response.body();
+        ObjectMapper mapper= new ObjectMapper();
+        List<Editorial> modelo = 
+                mapper.readValue(json, new TypeReference<List<Editorial>>() {});
+        
+        return modelo;
     }
     
     @WebMethod(operationName = "obtenerEditorial")
     public Editorial obtenerEditorial(
         @WebParam(name = "id") int id
-    ) {
-        return this.editorialBO.obtener(id);
+    ) throws IOException, InterruptedException {
+        String url = this.urlBase + "/" + this.NOMBRE_RESOURCE+ "/" + id;
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .GET()
+                .build();
+        
+        HttpResponse<String> response = 
+                client.send(request, HttpResponse.BodyHandlers.ofString());
+        String json = response.body();
+        ObjectMapper mapper= new ObjectMapper();
+        Editorial modelo = mapper.readValue(json, Editorial.class);
+        
+        return modelo;
     }
     
     @WebMethod(operationName = "eliminarEditorial")
     public void eliminarEditorial(
         @WebParam(name = "id") int id
-    ) {
-        this.editorialBO.eliminar(id);
+    ) throws IOException, InterruptedException {
+        String url = this.urlBase + "/" + this.NOMBRE_RESOURCE + "/" + id;
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(url))
+                .DELETE()
+                .build();
+        client.send(request, HttpResponse.BodyHandlers.ofString());
     }
     
     @WebMethod(operationName = "guardarEditorial")
     public void guardarEditorial(
-        @WebParam(name = "editorial") Editorial editorial, 
+        @WebParam(name = "editorial") Editorial modelo, 
         @WebParam(name = "estado") Estado estado
-    ) {
-        this.editorialBO.guardar(editorial, estado);
+    ) throws IOException, InterruptedException {
+        
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(modelo);
+
+        String url;
+        HttpRequest request;
+
+        if (estado == Estado.Nuevo) {
+            url = this.urlBase + "/" + this.NOMBRE_RESOURCE;
+            request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(json))
+                    .build();
+        } else {
+            url = this.urlBase + "/" + this.NOMBRE_RESOURCE + "/" + modelo.getIdEditorial();
+            request = HttpRequest.newBuilder()
+                    .uri(URI.create(url))
+                    .header("Content-Type", "application/json")
+                    .PUT(HttpRequest.BodyPublishers.ofString(json))
+                    .build();
+        }
+
+        client.send(request, HttpResponse.BodyHandlers.ofString());
     }
 }
